3f6933f8172062c4e17b940eef61b3b6fc305228
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-07-21 10:52:55 -0600

    feat: Se creo el model de producto

---
+++ EJERCICIOCLASE/src/models/producto.ts
@@ -0,0 +1,11 @@
+import { EstadoProducto } from "../models/estadoProducto";
+
+export interface Producto {
+    id: number;
+    nombre: string;
+    descripcion: string;
+    precio: number;
+    stock: number;
+    categoria: string;
+    estado: EstadoProducto;
+}
